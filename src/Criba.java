1e6b4071d372951f7570fda68e662dc8b3914d0a
Jordi-Allepuz <valencianu@gmail.com>
2023-03-02 19:16:10 +0100

    Commit 14- Extraemos los dos metodos para imprimir el vector de "iniciales" y el vector de "primos"

--- src/Criba.java
+++ src/Criba.java
@@ -60,23 +60,36 @@ public class Criba {
     }
 
 
+
+    public static void imprimirIniciales(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            if (i % 10 == 0) System.out.println();
+            System.out.print(i + 1 + "\t");
+        }
+    }
+
+    public static void imprimirPrimos(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            if (i % 10 == 0) System.out.println();
+            System.out.print(vector[i] + "\t");
+        }
+    }
+
+    
+   
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
         int dato = teclado.nextInt();
         int vector[] = new int[dato];
         System.out.println("\nVector inicial hasta :" + dato);
-        for (int i = 0; i < vector.length; i++) {
-            if (i % 10 == 0) System.out.println();
-            System.out.print(i + 1 + "\t");
-        }
+        imprimirIniciales(vector);
         vector = generarPrimos(dato);
         System.out.println("\nVector de primos hasta:" + dato);
-        for (int i = 0; i < vector.length; i++) {
-            if (i % 10 == 0) System.out.println();
-            System.out.print(vector[i] + "\t");
-        }
+        imprimirPrimos(vector);
     }
+
 }
 
 
